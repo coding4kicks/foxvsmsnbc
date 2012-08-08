e05157313632d6f428796d3d36d48e3237f23382
Scott Young <scott.r.young@hotmail.com>
2012-08-07 18:56:55 -0700

    Initial FOXvsMSNBC project version.

---
+++ js/fvm.js
@@ -0,0 +1,132 @@
+$(function () {
+
+    "use strict";
+
+    var
+        // variables for backbone and views
+        Word, Ranking, RankingView,
+        fox_view, msnbc_view, common_view,
+
+        // variables for scroller
+        counter     =   0,
+        count       =   10,
+        eightCommon =   false;
+
+	// Bind the tabs using jQuery Tools
+	$("#msnbccolumn ul.tabs").tabs("#msnbccolumn div.panes > div");
+	$("#foxcolumn ul.tabs").tabs("#foxcolumn div.panes > div");
+
+	/*
+	 * BACKBONE 
+	 */
+
+	////////////// MODEL //////////////
+	// Each word with its rank and count constitutes the model
+	Word = Backbone.Model.extend({
+		defaults: {
+			rank: 0,
+			word: "string",
+			count: 0
+		}
+	});
+
+	///////////// COLLECTION ///////////////////
+	// All the words for a site make up a ranking, or the common words with count equal to rank 2.
+    Ranking = Backbone.Collection.extend({
+		model: Word,
+
+		comparator: function (Word) {
+			return Word.get('rank');
+		}
+	});
+
+	//////////// VIEW //////////////////////
+	// A Ranking has its own view
+	RankingView = Backbone.View.extend({
+
+		// must set ({ el: $('xxx') }) during view creation
+
+		// Template for each wordBox
+		template: _.template("<div class='wordBox'><div class='rank'><h4><%= rank %></h4></div><div class='count'><h4><%= count %></h4></div><div class='word'><h3><%= word %></h3></div></div>"),
+
+		events: {
+			"click .wordBox" : "info"
+		},
+
+		initialize: function () {
+			_.bindAll(this, 'render', 'info'); // all functions that use this
+
+			// Create and fetch the collection
+			this.collection = new Ranking();
+			this.collection.url = 'rankings/' + this.el.id + ".json";  // set collections url to id of container
+			this.collection.fetch({success: this.render });
+		},
+
+		// Map each word into the template and append.	
+		render: function () {
+			var self = this;
+			_(this.collection.models).each(function (Word) {
+				var temp = self.template(Word.toJSON());
+				$(self.el).append(temp);
+			});
+			if ((this.el.id === "commonWords") && (_.size(this.collection.models)) > 8) { eightCommon = true; }
+			return this;
+		},
+
+		// Tooltip with the link information for the word.
+		info: function () {
+			return true;
+		}
+	});
+
+	// Create the views
+	fox_view = new RankingView({ el: $('#foxWordPane') });
+	msnbc_view = new RankingView({ el: $('#msnbcWordPane') });
+	common_view = new RankingView({ el: $('#commonWords') });
+
+    // Scroll Function
+	setInterval(function () {
+
+		// Select first word from each pane
+		var box1, box2, box3,
+		    bigBox1, bigBox2, bigBox3;
+		box1 = $("#msnbcWordPane .titleBox + div");
+		box2 = $("#foxWordPane .titleBox + div");
+		if (eightCommon) { box3 = $("#commonWords .titleBox + div"); }
+
+		// on every 20 counts move the top margin 1 pixel up
+		if (counter % 20 === 0) {
+		    box1.css("margin-top", count + "px");
+		    box2.css("margin-top", count + "px");
+		    if (eightCommon) {box3.css("margin-top", count + "px"); }
+		    count = count - 1;
+		}
+
+		counter = counter + 1;
+
+        // at 650 reset counters, delete word, and add to end
+		if (counter === 650) {
+
+			counter = 0;
+			count = 10;
+
+            bigBox1 = $("#msnbcWordPane");
+            bigBox2 = $("#foxWordPane");
+            if (eightCommon) { bigBox3 = $("#commonWords"); }
+
+			box1.remove();
+			box2.remove();
+			if (eightCommon) { box3.remove(); }
+
+			box1.css("margin-top", "10px");
+			box2.css("margin-top", "10px");
+			if (eightCommon) { box3.css("margin-top", "10px"); }
+
+			bigBox1.append(box1);
+			bigBox2.append(box2);
+			if (eightCommon) { bigBox3.append(box3); }
+		}
+	}, 1);
+
+});
+
---
+++ python/Spider.py
@@ -0,0 +1,192 @@
+"""	Web Crawler - an icky spider that crawls the user's site.
+		1) Only searches URLs with the user provided host name
+		2) Passes the page back to a concurrent process to index the site
+		3) Performs a Breadth First Search (BFS) - not BFFs
+		4) Has a max pages, indicates where to stop, so it't doesn't go crazy and index the whole web.
+		5) Pulls pages to search from the datastructure to which the parser adds them (need a buffer)
+"""
+import urllib
+import cPickle
+from Parser import *
+
+class Spider:
+    #############################
+    ## CONSTANTS AND VARIABLES
+    ###############################
+
+    ## URI or URL of the site
+    site_url = ''
+
+    ## List of saved content files
+    file_list = []
+
+    ## Queue of links to follow
+    link_queue = []
+
+    ## List of processed links
+    links_processed = []
+
+    ## Max pages to search
+    MAX = 10
+
+    ## the current document number
+    doc_num = 1
+
+    ## the current document name
+    doc_name = "doc1"
+
+    # A dictionary of links
+    dic_links = {}
+
+    #############################
+    ## METHODS: PROCEDURES AND FUNCTIONS
+    ###############################
+
+    """ Initialize Web Crawler - Constructor
+            Arguments 	- Site_Link (URI)
+            Returns		- Web Crawler
+    """
+    def __init__(self, site):
+
+            self.site_url = site
+
+    """ Start Web Crawler
+            Arguments 	- Web Crawler
+            Returns		- HTML Documents
+    """
+    def run(self):
+
+        ## try to get the initial page
+        try:
+
+            ## open and read the initial page's data
+            f = urllib.urlopen(self.site_url)
+            data = f.read()
+            f.close
+
+            ## create a parser and run it on the initial page
+            parser = Parser(self.site_url)
+            parser.run(data, self.doc_name)
+
+            ## get the initial links
+            self.link_queue = parser.get_links()
+
+            ## add url to processed list
+            self.links_processed += [self.site_url]
+
+            ## add to link dictionary
+            self.dic_links[self.doc_name] = self.site_url
+
+            ## increment the document number and name
+            self.doc_num = self.doc_num + 1
+            self.doc_name = "doc" + str(self.doc_num)
+
+#            print("Print the Links: Test")
+#            i = 0
+#            for link in self.link_queue:
+#                print(link)
+#                i = i + 1
+#                if i == 10:
+#                    break
+        except:
+            print("Problem with the initial page")
+
+        ## while start is less than max, index more pages
+        counter = 0
+        while counter < self.MAX:
+
+
+            ## get the next link
+            link = self.link_queue.pop(0)
+
+            if link not in self.links_processed:
+
+                ## try to process the page
+                try:
+
+                    ## open and read in the page
+                    f = urllib.urlopen(link)
+                    data = f.read()
+                    f.close
+
+                    ## create a parser and run it on the initial page
+                    parser.run(data, self.doc_name)
+
+                    ## get additional links
+                    self.link_queue += parser.get_links()
+
+                    ## add url to processed list
+                    self.links_processed += [link]
+
+                    ## increment the document number and name
+                    self.doc_num = self.doc_num + 1
+                    self.doc_name = "doc" + str(self.doc_num)
+
+                ## problem processing page
+                except:
+
+                        print("Problem processing: " + link)
+                        continue
+
+                ## increment counter
+                counter = counter + 1
+              
+                ## add to link dictionary
+                self.dic_links[self.doc_name] = self.site_url
+
+        ## get the file list
+        self.file_list = parser.get_file_list()
+
+        ## save the dictionary of links as an object for later reference
+        try:
+
+            ## save the index ***** LATER REPLACE index with SITE-NAME *****
+            outfile = open("Files/Links/link_dic.lk", "wb")
+            cPickle.dump(self.dic_links, outfile, 2)
+
+        except:
+
+            ## an error occured - probably opening a file
+            print("Problem Pickling the Links")
+
+        finally:
+
+            ## release the files
+            outfile.close()
+
+    """ Return File List
+            Arguments 	-
+            Returns	- File list
+    """
+    def get_file_list(self):
+
+        return self.file_list
+
+    """ Stop Web Crawler
+            Arguments 	- HTML Document
+            Returns		- Event Listerner
+    """
+
+    """ Return Documents
+            Arguments 	- HTML Document
+            Returns		- HTML Document
+    """
+
+    """ Switch Page
+            Arguments 	- None
+            Returns		- Query String (Obejct or String)
+    """
+
+    """ Queue Links
+            Arguments 	- Document, Host Name
+            Returns		- None: Adds Page to Queue (List)
+    """
+
+    """ Check Depth
+            Arguments 	- Document, Host Name
+            Returns		- None: Adds Page to Queue (List)
+    """
+
+    #############################
+    ## HELPER METHODS
+    ###############################
